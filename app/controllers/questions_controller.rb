5dbc4cee09362f009f8eaacdbdcfd23e48b29baa
Margie Nevarez and Marty Kovach <margie+marty@>
2017-02-20 11:12:59 -0800

    add create action and display in index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
   end
 
   def show
@@ -8,5 +9,20 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
-  
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:success] = "Question added!"
+      redirect_to questions_path
+    else
+      flash[:alert] = "Oops, something went wrong. Please try again."
+      render :new
+    end
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :description, :option_a, :option_b)
+  end
 end
